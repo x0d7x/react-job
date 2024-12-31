09be2f25aca2a42d34dd5fd052b417649458fac8
dullx <dullx@dullxs-MacBook-Pro.local>
2024-12-31 16:30:50 +0300

    chore: move css to src folder

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaExclamationTriangle } from "react-icons/fa";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaExclamationTriangle } from 'react-icons/fa';
 const NotFoundPage = () => {
-  return (
-    <section class="text-center flex flex-col justify-center items-center h-96">
-      <FaExclamationTriangle className="mb-4 text-yellow-400 text-8xl" />
-      <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p class="text-xl mb-5">This page does not exist</p>
-      <Link
-        to="/"
-        class="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
-      >
-        Go Back
-      </Link>
-    </section>
-  );
+	return (
+		<section className='text-center flex flex-col justify-center items-center h-96'>
+			<FaExclamationTriangle className='mb-4 text-yellow-400 text-8xl' />
+			<h1 className='text-6xl font-bold mb-4'>404 Not Found</h1>
+			<p className='text-xl mb-5'>This page does not exist</p>
+			<Link
+				to='/'
+				className='text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4'
+			>
+				Go Back
+			</Link>
+		</section>
+	);
 };
 
 export default NotFoundPage;
